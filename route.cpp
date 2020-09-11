b2694ff79e26d33a07536d4c96f1f478f8b46e4d
Ángel Bravo Sáenz <angelbravo@posteo.net>
2020-09-11 21:44:22 +0200

    muestra la matriz de rutas en pantalla

--- route.cpp
+++ route.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <stack>
 
 /*******************************
  * Clase para la base de datos *
@@ -13,15 +14,23 @@ class Database {
 		Database();
 		Database(unsigned int origin, unsigned int  destiny);
 		Database(unsigned int origin, unsigned int, unsigned int distance);
-		void start();
+		
+		void setRoutesOnDatabase();
 		void printVertex(unsigned int vertex);
+		long unsigned int numberOfRoutes();
+		void printDatabase();
+		
+		const unsigned int numOfVertex = 5;
 		enum Vertex { A, B, C, D, E};
+		
 		std::vector<Database> data_list;
+		
 	protected:
 		unsigned int initialVertex;
 		unsigned int finalVertex;
 		unsigned int vertexDistance;
 		unsigned int pos_ult;
+		
 		std::string vertex;
 };
 
@@ -42,20 +51,20 @@ Database::Database (unsigned int origin, unsigned int  destiny, unsigned int dis
 	pos_ult = 0;
 }
 
-void Database::start() {
-	Database walk1 = {A, B, 2}; //data_list[0]
-	data_list.push_back(walk1);
-	Database walk2 = {A, C, 3};
-	data_list.push_back(walk2);
-	Database walk3 = {C, D, 1};
-	data_list.push_back(walk3);
-	Database walk4 = {C, E, 4};
-	data_list.push_back(walk4);
-
+void Database::setRoutesOnDatabase() {
+	Database route1 = {A, B, 2}; //data_list[0]
+	data_list.push_back(route1);
+	Database route2 = {A, C, 3};
+	data_list.push_back(route2);
+	Database route3 = {C, D, 1};
+	data_list.push_back(route3);
+	Database route4 = {C, E, 4};
+	data_list.push_back(route4);
+	//unsigned int n = data_list.size();
+	//std::cout << data_list.size() << "\n"; <- Número de rutas
 	//printVertex(data_list[0].from); // A
-	
 }
-
+	
 void Database::printVertex(unsigned int value) {
 	switch (value) {
 		case 0 : {vertex.assign("A"); break;}
@@ -64,9 +73,43 @@ void Database::printVertex(unsigned int value) {
 		case 3 : {vertex.assign("D"); break;}
 		case 4 : {vertex.assign("E"); break;}
 	}
-	std::cout << vertex << "\n";
+	std::cout << vertex; // << "\n";
 }
 
+void Database::printDatabase() {
+	for(unsigned int i = 0; i < (numOfVertex-1); i++){
+		printVertex(data_list[i].initialVertex);
+		std::cout << ", ";
+		printVertex(data_list[i].finalVertex);
+		std::cout << ", " << 
+		data_list[i].vertexDistance 
+		<< "\n";
+		}
+	}
+
+long unsigned int Database::numberOfRoutes(){
+	return data_list.size();
+}
+	
+/**************************************
+ * Clase para la matriz de adyacencia *
+ **************************************/
+ //¿Herencia?
+class AdjacencyMatrix {
+	public:
+		AdjacencyMatrix();
+		void transformIntoMatrix();
+	private:
+		std::vector<Database> matrix;
+		std::vector<unsigned int> distance;	
+	};
+
+void AdjacencyMatrix::transformIntoMatrix() {
+	//unsigned int adyacMatrix[data_list.size()][numOfVertex+1];
+	
+	}
+	
+	
 /**********************
  * Clase para la pila *
  **********************/
@@ -90,11 +133,17 @@ class Pila {
  ********************/
 
 /*Prototipos*/
+bool areConnected();
+
 
 int main (){
+	Database db;
 	unsigned int origin;
 	unsigned int destiny;
-	std::vector<Database> vectorEntrada;
+	std::stack<unsigned int> stack;
+	std::vector<unsigned int> vector;
+	
+	db.setRoutesOnDatabase();
 	
 	std::cout << "Programa de búsqueda." << "\n";
 	std::cout << "versión 0.0.1." << "\n";
@@ -106,10 +155,33 @@ int main (){
 	std::cout << "Selecciona el vértice destino: ";
 	std::cin >> destiny;
 
+	std::cout << "\n";
+	
+	std::cout << "Número de entradas en la base de datos = " << db.numberOfRoutes() << "\n";
+	std::cout << "\n";
+	
+	std::cout << "Matriz de grafos." << "\n";
+	db.printDatabase();
+	std::cout << "\n";
+	
+	std::cout << "Matriz de adyacencia." << "\n";
 	
 
-	Database db(origin, destiny);
-	db.start();
+	}
 	
+	
+bool areConnected() {
+	bool connection = false;
+	
+	/*unidas(desde, hacia)
+char *desde, *hacia;
+{
+	register int t;
 
+	for(t=pos_ult-1; t>1; t--)
+		if(!strcmp(vuelos[t].desde, desde) &&
+		!strcmp(vuelos[t].hacia, hacia)) return vuelos[t].distancia;
+		return 0;
+}*/
+	return connection;
 	}
